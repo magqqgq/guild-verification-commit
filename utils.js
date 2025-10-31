7a4bb4cb9f6fb538baa5562470a69be546292145
magqqgq <146786427+magqqgq@users.noreply.github.com>
2025-10-31 11:22:25 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T08:22:21.292Z
-Commit: Implement user profile customization
+Timestamp: 2025-10-31T08:22:25.220Z
+Commit: Fix memory leak in event handlers
