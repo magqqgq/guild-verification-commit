a6e92270d6041bd3fd4fa2b4091743e0deaa6b39
magqqgq <146786427+magqqgq@users.noreply.github.com>
2025-10-31 11:41:26 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T08:22:25.220Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-10-31T08:41:26.723Z
+Commit: Fix CSS styling bug on mobile devices
